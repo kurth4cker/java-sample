cdd8147aee2eb8a931a80144473d0e8e2082af4d
kurth4cker <kurth4cker@gmail.com>
2024-05-17 10:21:08 +0300

    kthr.Vector: new class
    Added as template with lots of TODO marked functions

---
+++ src/kthr/Vector.java
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package kthr;
+
+public final class Vector {
+    private Object[] array;
+    private int index;
+
+    public Vector()
+    {
+        // TODO
+    }
+
+    public Vector(int capacity)
+    {
+        // TODO
+    }
+
+    public void add(Object obj)
+    {
+        // TODO
+    }
+
+    public void add(int index, Object obj)
+    {
+        // TODO
+    }
+
+    public void clear()
+    {
+        // TODO
+    }
+
+    public Object set(int index, Object obj)
+    {
+        // TODO
+        Object oldObj = array[index];
+
+        // TODO
+
+        return oldObj;
+    }
+
+    public void trimToSize()
+    {
+        // TODO
+    }
+}
