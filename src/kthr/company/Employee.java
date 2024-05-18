cca8736ec8383c9e05221d8529fee4693f15b4c3
kurth4cker <kurth4cker@gmail.com>
2024-05-18 15:00:27 +0300

    kthr.**: change license header comment style

--- src/kthr/company/Employee.java
+++ src/kthr/company/Employee.java
@@ -1,7 +1,5 @@
-/*
- * SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
+// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 package kthr.company;
 
