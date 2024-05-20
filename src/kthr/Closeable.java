eef5a4921e7776638d6e6d7025c61557fce9a446
kurth4cker <kurth4cker@gmail.com>
2024-05-20 04:46:23 +0300

    kthr.Closeable: new interface

---
+++ src/kthr/Closeable.java
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package kthr;
+
+public interface Closeable extends java.io.Closeable {
+    void close();
+}
