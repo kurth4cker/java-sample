669bffab4dac5ea07fbf5f00451606896b353213
kurth4cker <kurth4cker@gmail.com>
2024-05-21 19:42:00 +0300

    kthr.Property: new class

---
+++ src/kthr/Property.java
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
+
+package kthr;
+
+final class Property {
+    public static void print(String property)
+    {
+        System.out.printf("%s = %s", property, System.getProperty(property));
+    }
+
+    public static void println(String property)
+    {
+        print(property);
+        System.out.println();
+    }
+}
