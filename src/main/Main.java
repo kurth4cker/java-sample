72fa52ff57d306d94539df6e790a59b457de2a78
kurth4cker <kurth4cker@gmail.com>
2024-07-18 09:36:34 +0300

    main.Main: move from kthr.Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
 
-package kthr;
+package main;
 
 final class Main {
     public static void main(String[] args)
