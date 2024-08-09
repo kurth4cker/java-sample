c230263dfea984cdaa5bc0e1c51134cb0df3ca5d
kurth4cker <kurth4cker@gmail.com>
2024-08-09 15:40:01 +0300

    remove some classes

--- src/kthr/Closeable.java
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
-
-package kthr;
-
-public interface Closeable extends java.io.Closeable {
-    void close();
-}
