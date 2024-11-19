28a06c38203f6dc91fba1d482c393cd73e73809f
kurth4cker <kurth4cker@gmail.com>
2024-11-19 09:16:42 +0300

    Initialize maven

---
+++ src/main/java/site/srht/kurth4cker/HelloUtil.java
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
+
+package site.srht.kurth4cker;
+
+public final class HelloUtil {
+    private HelloUtil() {}
+}
---
+++ src/test/java/site/srht/kurth4cker/HelloUtilTest.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
+
+package site.srht.kurth4cker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloUtilTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
